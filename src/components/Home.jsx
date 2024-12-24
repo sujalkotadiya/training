27f08fac369491bbdb75c9d3b837092da0d63fca
sujal kotadiya <sujalkotadiya762@gmail.com>
2024-12-24 11:43:59 +0530

    Updated files with required changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,51 @@
 import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
 
 const Home = () => {
-const [title,setTitle] =useState('')
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState('')
+  const [serchParams, setSearchParams] = useSearchParams()
+  const pasteId = serchParams.get("pasteId")
+
+  function createPaste() {
+    const paste={
+      title:title,
+      content:value,
+      _id:pasteId || 
+      Date.now().toString(36),
+      createdAt:new Date().toISOString()
+    }
+  }
 
   return (
     <div>
-      <input 
-      className="p-2 rounded-2xl mt-2"
-      type="text"
-      placeholder="enter title here"
-      value={title}
-      onChange={(e)=>setTitle(e.target.value)}
-      />
+      <div className="flex flex-row gap-7">
+        <input
+          className="p-1 rounded-2xl mt-2 w-[66%] pl-4"
+          type="text"
+          placeholder="enter title here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-<button>
-  Create My Paste
-</button>
+        <button
+        onClick={createPaste}
+        className="p-2 rounded-2xl mt-2">
+          {
+            pasteId ? "Update My Paste" : "Add My Paste"
+          }
+        </button>
 
+      </div>
+      <div className="mt-8">
+        <textarea
+        className="rounded-2xl mt-4 min-w-[500px] p-4"
+          value={value}
+          placeholder="Enter Content Here"
+          onChange={(e) => setValue(e.target.value)}
+          rows={20}
+        />
+      </div>
     </div>
   )
 }
