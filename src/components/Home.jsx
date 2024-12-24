46e5ca0f578e1af997e08f3fd00d4335b5dd3ccf
sujal kotadiya <sujalkotadiya762@gmail.com>
2024-12-24 11:14:04 +0530

    Updated files with required changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from "react"
 
 const Home = () => {
+const [title,setTitle] =useState('')
+
   return (
     <div>
-      Home
+      <input 
+      className="p-2 rounded-2xl mt-2"
+      type="text"
+      placeholder="enter title here"
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)}
+      />
+
+<button>
+  Create My Paste
+</button>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='flex flex-row gap-4 '>
+      <NavLink to="/">
+        Home
+      </NavLink>
+      <NavLink to="/pastes">
+        Pastes
+      </NavLink>
     </div>
   )
 }
