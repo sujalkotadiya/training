ad1fdd816dae5de6d0920ea7ca3d1aa56c099429
sujal kotadiya <sujalkotadiya762@gmail.com>
2024-12-25 14:36:49 +0530

    Updated files with required changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react"
+import { useDispatch } from "react-redux"
 import { useSearchParams } from "react-router-dom"
+import { addToPastes, updateToPastes } from "../redux/pasteSlice"
 
 const Home = () => {
   const [title, setTitle] = useState('')
   const [value, setValue] = useState('')
   const [serchParams, setSearchParams] = useSearchParams()
   const pasteId = serchParams.get("pasteId")
+  const dispatch = useDispatch()
 
   function createPaste() {
-    const paste={
-      title:title,
-      content:value,
-      _id:pasteId || 
-      Date.now().toString(36),
-      createdAt:new Date().toISOString()
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId ||
+        Date.now().toString(36),
+      createdAt: new Date().toISOString()
     }
+    if (pasteId) {
+      dispatch(updateToPastes(paste))
+    } else {
+      dispatch(addToPastes(paste))
+    }
+
+    setTitle('')
+    setValue('')
+    setSearchParams({})
+
   }
+
+
 
   return (
     <div>
@@ -29,8 +44,8 @@ const Home = () => {
         />
 
         <button
-        onClick={createPaste}
-        className="p-2 rounded-2xl mt-2">
+          onClick={createPaste}
+          className="p-2 rounded-2xl mt-2">
           {
             pasteId ? "Update My Paste" : "Add My Paste"
           }
@@ -39,7 +54,7 @@ const Home = () => {
       </div>
       <div className="mt-8">
         <textarea
-        className="rounded-2xl mt-4 min-w-[500px] p-4"
+          className="rounded-2xl mt-4 min-w-[500px] p-4"
           value={value}
           placeholder="Enter Content Here"
           onChange={(e) => setValue(e.target.value)}
--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+
 const paste = () => {
+
+  const pastes = useSelector((state) => state.paste.pastes);
+  console.log(pastes)
+  const [searchTerm, setSearchTerm] = useState('')
+  // const dispatch = useDispatch()
+
+  const filteredData = pastes.filter((paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
+
   return (
     <div>
-      paste
+      <input
+        className="p-2 rounded-2xl min-w-[600px] mt-5"
+        type="search"
+        placeholder="search here"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="flex flex-col gap-5 mt-5">
+        {filteredData.length > 0 &&
+          filteredData.map(
+            (paste) => {
+              return (
+                <div key={paste.id}
+                  className="border ">
+                  <div>
+                    {paste.title}
+                  </div>
+                  <div>
+                    {paste.content}
+                  </div>
+                  <div className="flex flex-row gap-4 justify-evenly">
+                    <button>Edit</button>
+                    <button>View</button>
+                    <button>Delete</button>
+                    <button>Copy</button>
+                    <button>Share</button>
+                  </div>
+                  <div>
+                    {paste.createdAt}
+                  </div>
+                </div>
+              )
+            }
+          )
+        }
+      </div>
     </div>
   )
 }
